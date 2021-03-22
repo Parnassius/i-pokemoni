05222cd78380c9827dcd90c2dcd22d4b6432f57a
Parnassius <Parnassius@users.noreply.github.com>
2021-03-22 17:16:58 +0100

    paths

--- paths.py
+++ paths.py
@@ -3,32 +3,10 @@ from os.path import dirname, join
 BASE_PATH = join(dirname(__file__), "..", "pokemon_data")
 
 PATHS = {
-    "pikachu": join(
-        BASE_PATH,
-        "lgpe131072",
-        "lower",
-        "Pokémon: Let’s Go, Pikachu! [v131072]",
-        "RomFS",
-    ),
-    "eevee": join(
-        BASE_PATH,
-        "lgpe131072",
-        "lower",
-        "Pokémon: Let’s Go, Eevee! [v131072]",
-        "RomFS",
-    ),
-    "sword": join(
-        BASE_PATH,
-        "swsh393216",
-        "lower",
-        "Pokémon Shield [v393216]",
-        "RomFS",
-    ),
-    "shield": join(
-        BASE_PATH,
-        "swsh393216",
-        "lower",
-        "Pokémon Sword [v393216]",
-        "RomFS",
-    ),
+    # fmt: off
+    "ultrasun": join(BASE_PATH, "usum12", "lower", "Pokémon Ultra Sun [v1.2]", "romfs"),
+    "pikachu": join(BASE_PATH, "lgpe131072", "lower", "Pokémon: Let’s Go, Pikachu! [v131072]", "RomFS"),
+    "eevee": join(BASE_PATH, "lgpe131072", "lower", "Pokémon: Let’s Go, Eevee! [v131072]", "RomFS"),
+    "sword": join(BASE_PATH, "swsh393216", "lower", "Pokémon Shield [v393216]", "RomFS"),
+    "shield": join(BASE_PATH, "swsh393216", "lower", "Pokémon Sword [v393216]", "RomFS"),
 }
